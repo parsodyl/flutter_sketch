7debea8fa4356ebdf9e92ef66e64471c1eb11a53
Daniele Paradiso <daniele.paradiso3@gmail.com>
2023-03-22 12:29:34 +0100

    removed old util package

--- packages/flutter_sketch_util/example/flutter_sketch_util_example.dart
+++
@@ -1,5 +0,0 @@
-//import 'package:flutter_sketch_util/flutter_sketch_util.dart';
-
-void main() {
-  // example
-}
--- packages/flutter_sketch_util/lib/flutter_sketch_util.dart
+++
@@ -1,5 +0,0 @@
-library flutter_sketch_util;
-
-export 'src/math_util.dart';
-export 'src/noise_util.dart';
-export 'src/random_util.dart';
